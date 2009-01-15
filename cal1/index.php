77c39c59df26580ecaceb5889bd82fc78feef224
Dav Glass <davglass@gmail.com>
2009-01-15 07:01:12 -0800

    Updated example to latest YUI release

--- cal1/index.php
+++ cal1/index.php
@@ -2,12 +2,15 @@
 <html>
 <head>
     <title>Calendar Row Select</title>
-    <link rel="stylesheet" href="http://blog.davglass.com/files/yui/css/yuicss.css" type="text/css">
+    <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.6.0/build/reset-fonts-grids/reset-fonts-grids.css"> 
+    <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.6.0/build/assets/skins/sam/skin.css">     
     <link rel="stylesheet" href="http://blog.davglass.com/wp-content/themes/davglass/style.css" type="text/css">
-    <link rel="stylesheet" type="text/css" href="../css/dpSyntaxHighlighter.css">
+    <link rel="stylesheet" type="text/css" href="http://developer.yahoo.com/yui/assets/dpSyntaxHighlighter.css">
     <style type="text/css" media="screen">
-		@import url( ../css/calendar_assets/calendar.css );
-/* highlight the whole row */
+        p, h2 {
+            margin: 1em;
+        }
+        /* highlight the whole row */
 		tr.hilite-row td.calcell {
 			background-color:yellow;
 		}
@@ -35,126 +38,156 @@
         }
 	</style>
 </head>
-<body>
+<body class="yui-skin-sam">
 <div id="davdoc" class="yui-t7">
-    <div id="hd"><h1 id="header"><a href="http://blog.davglass.com/">YUI: Calendar Select Whole Row (YUI Version .12)</a></h1></div>
+    <div id="hd"><h1 id="header"><a href="http://blog.davglass.com/">YUI: Calendar Select Whole Row</a></h1></div>
 
     <div id="bd">
-    <p>Updated this example to work with YUI Version .12. <a href="index11.php">The original can be found here</a>.</p>
+    <p>Updated this example to work with YUI Version 2.6.0.</p>
     <p>Click on a date to select the entire row.</p>
-    <p><a href="javascript:alert(cal1.getSelectedDates());">What is selected?</a></p>
+    <p><a href="#" id="showSelected">What is selected?</a></p>
     <div id="cal1Container"></div><br clear="all">
     <textarea name="code" class="JScript">
-var cal1;
-function init() {
-    cal1 = new YAHOO.widget.Calendar('cal1', 'cal1Container', {
-        MULTI_SELECT: true
+(function() {
+    var Dom = YAHOO.util.Dom,
+        Event = YAHOO.util.Event,
+        cal1 = null;
+
+
+    Event.onDOMReady(function() {
+        cal1 = new YAHOO.widget.Calendar('cal1', 'cal1Container', {
+            MULTI_SELECT: true
+        });
+        
+        cal1.selectEvent.subscribe(rowSelect, cal1, true);
+        cal1.render();
     });
-    
-    cal1.selectEvent.subscribe(rowSelect, cal1, true);
-    cal1.render();
-}
 
-function rowSelect(ev, arr) {
-    /* Deselect all items */
-    this.deselectAll();
-    /* Remove the selected class */
-    $D.removeClass(this.cells, 'selected');
-    /* Prep the array of selected items */
-    var selected = [];
-    /* Find the cell index of the selected date */
-    for (var i = 0; i < this.cellDates.length; i++) {
-        if (this._fieldArraysAreEqual(arr[0][0], this.cellDates[i])) {
-            var index = i;
-            break;
-        }
-    }
-    /* The TR parent of the selected date */
-    var row = this.cells[index].parentNode;
-    /* All of the TDs in that row */
-    var tds = row.getElementsByTagName('td');
-    for (var i = 0; i < tds.length; i++) {
-        var cell = tds[i];
-        /* Get the cell index of this TD */
-        var index = parseInt(cell.id.replace(this.id + '_cell', ''));
-        /* The date of this TD */
-        var d = this.cellDates[index];
-        var dCellDate = this._toDate(d);
-        /* This check can be removed if you want them to be able to select days outside of the current month */
-        if (!this.isDateOOM(dCellDate)) {
-            var selectDate = d.concat();
-            /* Add this date to the selected array */
-            selected[selected.length] = selectDate;
-            /* Set selected style */
-            this.renderCellStyleSelected(dCellDate, cell);
-        }
-    }
-    /* Update the config with the array of selected dates */
-    this.cfg.setProperty("selected", selected);
-}
+    Event.on('showSelected', 'click', function(e) {
+        Event.stopEvent(e);
+        alert(cal1.getSelectedDates());
+    });
 
-$E.addListener(window, 'load', init);
+
+
+    var rowSelect = function(ev, arr) {
+        /* Deselect all items */
+        this.deselectAll();
+        /* Remove the selected class */
+        Dom.removeClass(this.cells, 'selected');
+        /* Prep the array of selected items */
+        var selected = [], index = 0;
+        /* Find the cell index of the selected date */
+        for (var i = 0; i &lt; this.cellDates.length; i++) {
+            if (this._fieldArraysAreEqual(arr[0][0], this.cellDates[i])) {
+                index = i;
+                break;
+            }
+        }
+        /* The TR parent of the selected date */
+        var row = this.cells[index].parentNode,
+            /* All of the TDs in that row */
+            tds = row.getElementsByTagName('td');
+
+        for (var i = 0; i &lt; tds.length; i++) {
+            var cell = tds[i],
+            /* Get the cell index of this TD */
+                index = parseInt(cell.id.replace(this.id + '_cell', '')),
+                /* The date of this TD */
+                d = this.cellDates[index],
+                dCellDate = this._toDate(d);
+
+            /* This check can be removed if you want them to be able to select days outside of the current month */
+            if (!this.isDateOOM(dCellDate)) {
+                var selectDate = d.concat();
+                /* Add this date to the selected array */
+                selected[selected.length] = selectDate;
+                /* Set selected style */
+                this.renderCellStyleSelected(dCellDate, cell);
+            }
+        }
+        /* Update the config with the array of selected dates */
+        this.cfg.setProperty("selected", selected);
+    }
+
+    dp.SyntaxHighlighter.HighlightAll('code');
+})();
     </textarea>
 </div>
 <div id="ft">&nbsp;</div>
 </div>
-<script type="text/javascript" src="../js/yui-012.js"></script>
-<script type="text/javascript" src="../tools/tools-min.js"></script>
-<script src="../js/dpSyntaxHighlighter.js"></script>
+<script type="text/javascript" src="http://yui.yahooapis.com/2.6.0/build/utilities/utilities.js"></script> 
+<script type="text/javascript" src="http://yui.yahooapis.com/2.6.0/build/calendar/calendar-min.js"></script> 
+<script src="http://developer.yahoo.com/yui/assets/dpSyntaxHighlighter.js"></script>
+<script type="text/javascript" src="../js/toolseffects-min.js"></script>
 <script type="text/javascript" src="../js/davglass.js"></script>
-<script type="text/javascript" src="../js/calendar-min.js"></script>
+
 <script type="text/javascript">
-var cal1;
-function init() {
-    cal1 = new YAHOO.widget.Calendar('cal1', 'cal1Container', {
-        MULTI_SELECT: true
+(function() {
+    var Dom = YAHOO.util.Dom,
+        Event = YAHOO.util.Event,
+        cal1 = null;
+
+
+    Event.onDOMReady(function() {
+        cal1 = new YAHOO.widget.Calendar('cal1', 'cal1Container', {
+            MULTI_SELECT: true
+        });
+        
+        cal1.selectEvent.subscribe(rowSelect, cal1, true);
+        cal1.render();
     });
-    
-    cal1.selectEvent.subscribe(rowSelect, cal1, true);
-    cal1.render();
 
-    dp.SyntaxHighlighter.HighlightAll('code'); 
-}
+    Event.on('showSelected', 'click', function(e) {
+        Event.stopEvent(e);
+        alert(cal1.getSelectedDates());
+    });
 
-function rowSelect(ev, arr) {
-    /* Deselect all items */
-    this.deselectAll();
-    /* Remove the selected class */
-    $D.removeClass(this.cells, 'selected');
-    /* Prep the array of selected items */
-    var selected = [];
-    /* Find the cell index of the selected date */
-    for (var i = 0; i < this.cellDates.length; i++) {
-        if (this._fieldArraysAreEqual(arr[0][0], this.cellDates[i])) {
-            var index = i;
-            break;
+
+
+    var rowSelect = function(ev, arr) {
+        /* Deselect all items */
+        this.deselectAll();
+        /* Remove the selected class */
+        Dom.removeClass(this.cells, 'selected');
+        /* Prep the array of selected items */
+        var selected = [], index = 0;
+        /* Find the cell index of the selected date */
+        for (var i = 0; i < this.cellDates.length; i++) {
+            if (this._fieldArraysAreEqual(arr[0][0], this.cellDates[i])) {
+                index = i;
+                break;
+            }
         }
-    }
-    /* The TR parent of the selected date */
-    var row = this.cells[index].parentNode;
-    /* All of the TDs in that row */
-    var tds = row.getElementsByTagName('td');
-    for (var i = 0; i < tds.length; i++) {
-        var cell = tds[i];
-        /* Get the cell index of this TD */
-        var index = parseInt(cell.id.replace(this.id + '_cell', ''));
-        /* The date of this TD */
-        var d = this.cellDates[index];
-        var dCellDate = this._toDate(d);
-        /* This check can be removed if you want them to be able to select days outside of the current month */
-        if (!this.isDateOOM(dCellDate)) {
-            var selectDate = d.concat();
-            /* Add this date to the selected array */
-            selected[selected.length] = selectDate;
-            /* Set selected style */
-            this.renderCellStyleSelected(dCellDate, cell);
-        }
-    }
-    /* Update the config with the array of selected dates */
-    this.cfg.setProperty("selected", selected);
-}
+        /* The TR parent of the selected date */
+        var row = this.cells[index].parentNode,
+            /* All of the TDs in that row */
+            tds = row.getElementsByTagName('td');
 
-$E.addListener(window, 'load', init);
+        for (var i = 0; i < tds.length; i++) {
+            var cell = tds[i],
+            /* Get the cell index of this TD */
+                index = parseInt(cell.id.replace(this.id + '_cell', '')),
+                /* The date of this TD */
+                d = this.cellDates[index],
+                dCellDate = this._toDate(d);
+
+            /* This check can be removed if you want them to be able to select days outside of the current month */
+            if (!this.isDateOOM(dCellDate)) {
+                var selectDate = d.concat();
+                /* Add this date to the selected array */
+                selected[selected.length] = selectDate;
+                /* Set selected style */
+                this.renderCellStyleSelected(dCellDate, cell);
+            }
+        }
+        /* Update the config with the array of selected dates */
+        this.cfg.setProperty("selected", selected);
+    }
+
+    dp.SyntaxHighlighter.HighlightAll('code');
+})();
+
 </script>
 </body>
 </html>
