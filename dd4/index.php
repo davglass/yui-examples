a7dadabd0c423b65224c18e56ea87d4dddc1b15f
Dav Glass <davglass@gmail.com>
2009-01-24 21:39:33 -0800

    Updated example dd4

--- dd4/index.php
+++ dd4/index.php
@@ -1,11 +1,12 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
-    <title>YUI: DragDrop Example</title>
-    <link rel="stylesheet" href="http://blog.davglass.com/files/yui/css/reset-min.css" type="text/css">
-            <link rel="stylesheet" href="http://blog.davglass.com/files/yui/css/fonts-min.css" type="text/css">
-            <link rel="stylesheet" href="http://blog.davglass.com/files/yui/css/grids-min.css" type="text/css">
-            <link rel="stylesheet" href="http://blog.davglass.com/wp-content/themes/davglass/style.css" type="text/css">
+    <title>YUI: DragDrop: Start and End Positions</title>
+    <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.6.0/build/reset-fonts-grids/reset-fonts-grids.css"> 
+    <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.6.0/build/assets/skins/sam/skin.css">     
+    <link rel="stylesheet" href="http://blog.davglass.com/wp-content/themes/davglass/style.css" type="text/css">
+    <link rel="stylesheet" type="text/css" href="http://developer.yahoo.com/yui/assets/dpSyntaxHighlighter.css">
+    
     <style type="text/css" media="screen">
         @import url( style.css );
 
@@ -25,56 +26,90 @@
         #davdoc {
             position: relative;
         }
+        #bd h2 {
+            margin-top: 30em;
+        }
 	</style>
 </head>
 <body>
 <div id="davdoc" class="yui-t7">
-    <div id="hd"><h1 id="header"><a href="http://blog.davglass.com/">YUI: Drag Drop Example</a></h1></div>
+    <div id="hd"><h1 id="header"><a href="http://blog.davglass.com/">YUI: DragDrop: Start and End Positions</a></h1></div>
     <div id="bd">
-    <div id="dropper">
-    Drage me around and drop me
-    </div>
-    <div id="results"></div>
+        <p>This example shows how to get the start and end positions from a DragDrop object.</p>
+        <div id="dropper">
+        Drag me around and drop me
+        </div>
+        <div id="results"></div>
+        <h2>The Javascript</h2>
+        <textarea name="code" class="JScript">
+(function() {
+    var Event = YAHOO.util.Event,
+        Dom = YAHOO.util.Dom,
+        ddTar = null,
+        res = Dom.get('results'),
+        startPoints = null;
+
+
+    var handleEnd = function(ev) {
+        var tar = Event.getTarget(ev),
+            str = 'Starting X: ' + startPoints[0] + '<br>Starting Y: ' + startPoints[1] + '<br><br>',
+            xy = Dom.getXY(tar);
+
+        str += 'Current X: ' + xy[0] + '<br>Current Y: ' + xy[1] + '<br>';
+        startPoints = null;
+        res.innerHTML = str;
+    };
+
+    var handleStart = function() {
+        startPoints = Dom.getXY('dropper');
+    };
+
+    Event.onDOMReady(function() {
+        drop = new YAHOO.util.DD('dropper');
+        drop.endDrag = handleEnd;
+        drop.b4StartDrag = handleStart;
+    });
+
+})();
+        </textarea>
     </div>
 </div>
-<script type="text/javascript" src="../js/yahoo-min.js"></script>
-<script type="text/javascript" src="../js/dom-min.js"></script>
-<script type="text/javascript" src="../js/event-min.js"></script>
-<script type="text/javascript" src="../js/animation-min.js"></script>
-<script type="text/javascript" src="../js/dragdrop-min.js"></script>
+<script type="text/javascript" src="http://yui.yahooapis.com/2.6.0/build/utilities/utilities.js"></script>
+<script src="http://developer.yahoo.com/yui/assets/dpSyntaxHighlighter.js"></script>
+<script type="text/javascript" src="../js/toolseffects-min.js"></script>
+<script type="text/javascript" src="../js/davglass.js"></script>
 <script type="text/javascript">
+(function() {
+    var Event = YAHOO.util.Event,
+        Dom = YAHOO.util.Dom,
+        ddTar = null,
+        res = Dom.get('results'),
+        startPoints = null;
 
-var ddTar;
-var res = YAHOO.util.Dom.get('results');
-var startPoints = null;
 
+    var handleEnd = function(ev) {
+        var tar = Event.getTarget(ev),
+            str = 'Starting X: ' + startPoints[0] + '<br>Starting Y: ' + startPoints[1] + '<br><br>',
+            xy = Dom.getXY(tar);
 
-function handleEnd(ev) {
-    var tar = YAHOO.util.Event.getTarget(ev);
-    var str = 'Starting X: ' + startPoints[0] + '<br>Starting Y: ' + startPoints[1] + '<br><br>';
-    var xy = YAHOO.util.Dom.getXY(tar);
-    str += 'Current X: ' + xy[0] + '<br>Current Y: ' + xy[1] + '<br>';
-    startPoints = null;
-    res.innerHTML = str;
-}
+        str += 'Current X: ' + xy[0] + '<br>Current Y: ' + xy[1] + '<br>';
+        startPoints = null;
+        res.innerHTML = str;
+    };
 
-function init() {
-    drop = new YAHOO.util.DD('dropper');
-    drop.endDrag = handleEnd;
-    drop.onMouseUp = handleUp;
-    drop.startDrag = handleStart;
-}
+    var handleStart = function() {
+        startPoints = Dom.getXY('dropper');
+    };
 
-YAHOO.util.Event.addListener(window, 'load', init);
+    Event.onDOMReady(function() {
+        drop = new YAHOO.util.DD('dropper');
+        drop.endDrag = handleEnd;
+        drop.b4StartDrag = handleStart;
+    });
 
-function handleStart(x,y) {
-    startPoints = [x, y];
-}
+    dp.SyntaxHighlighter.HighlightAll('code');
 
-function handleUp(ev) {
-    init();
-}
-
+})();
 
 </script>
 </body>
